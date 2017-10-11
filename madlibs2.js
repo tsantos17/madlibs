784042a32b81e75def92da3dcc54ae00bb44bb03
tsantos17 <31961885+tsantos17@users.noreply.github.com>
2017-10-11 09:16:16 -0400

    Add files via upload

--- madlibs2.js
+++ madlibs2.js
@@ -2,28 +2,26 @@
 var sentence = [];
 var replacements = [];
 var index = 0;
-filler(sentence, "Next Word" );
-filler(replacements, "Next Position");
-replace(sentence, replacements, "Part if speech for word in sentence");
 
+filler(sentence, "Next Word");
+filler(replacements, "Next Position");
+replace(sentence,replacements,"Part of speech for word in sentence ");
 alert("Switch positions");
+
 function filler (myArray,myPrompt){
-index = -1;
-do{
-	index++;
-	myArray[index] = prompt(myPrompt+"["+index+"] = ");
-}
-while(myArray[index] != "^");
-myArray.pop();
-console.log(myArray);
+	index = -1;
+	do{
+		index++;
+		myArray[index] = prompt(myPrompt+"["+index+"] = ");
+	}
+	while(myArray[index] != "^");
+	myArray.pop();
+	console.log(myArray);
 }
 
 function replace (mySArray,myRArray,myPrompt){
-	for(index = 0; index < myRArray.length;index++){
-		mySArray[myRArray[index]] = prompt(myPrompt+")
-
-
-}
-
-
+	for (index=0;index<myRArray.length;index++){
+		mySArray[myRArray[index]] = prompt(myPrompt+"["+[myRArray[index]]+"] = ");
+	}
+	console.log(mySArray);
 }
